aa3e28208ac596aa1f9459e60dee8611c0d176d4
penggongxi <penggongxi@edsz9.com>
2024-11-15 23:05:07 +0800

    feat: handle login and logout logic with TanStack Query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
 import HomePage from './pages/home/HomePage'
 import LoginPage from './pages/auth/login/LoginPage'
 import SignupPage from './pages/auth/signup/SignupPage'
@@ -6,19 +9,48 @@ import Sidebar from './components/common/SideBar'
 import RightPanel from './components/common/RightPanel'
 import NotificationPage from './pages/notification/NotificationPage'
 import ProfilePage from './pages/profile/ProfilePage'
+import LoadingSpinner from "./components/common/LoadingSpinner"
 function App() {
+
+  const { data: authUser, isLoading } = useQuery({
+    // queryKey 是可以在其他页面进行复用的
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const res = await fetch('/api/auth/getUser')
+        const data = await res.json()
+        if (data.error) return null
+        if (!res.ok) {
+          throw new Error(data.error || 'Something went wrong')
+        }
+        return data
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    retry: false,
+  })
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    )
+  }
 
   return (
     <div className="flex max-w-6xl mx-auto">
-      <Sidebar />
+      { authUser && <Sidebar />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path='/notifications' element={<NotificationPage />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path="/login" element={authUser ? <Navigate to='/' /> : <LoginPage />} />
+        <Route path="/signup" element={authUser ? <Navigate to='/' /> : <SignupPage />} />
+        <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+        <Route path="/profile/:username" element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
       </Routes>
-      <RightPanel />
+      { authUser && <RightPanel />}
+      <Toaster />
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false // 重新切回窗口时，不需要获取最新的数据
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
